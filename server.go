a9d16f29fff69d31efa13f3af57790fa75566af3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-06 23:51:35 +0800

    update NewServer about switch TLS mode like acme and static.

--- server.go
+++ server.go
@@ -1,8 +1,58 @@
 package msocks
 
+import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"net"
+
+	"github.com/For-ACGN/autocert"
+)
+
+const (
+	TLSModeACME   = "acme"
+	TLSModeStatic = "static"
+)
+
+// Server is a SOCKS-over-HTTPS server.
 type Server struct {
+	passHash string
+	logger   *logger
 }
 
-func NewServer() {
+// NewServer is used to create a SoH server.
+func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
+	logger, err := newLogger(config.Common.LogPath)
+	if err != nil {
+		return nil, err
+	}
 
+	maxConns := config.HTTP.MaxConns
+	if maxConns < 1 {
+		maxConns = 1000
+	}
+
+	network := config.HTTP.Network
+	address := config.HTTP.Address
+	var listener net.Listener
+	switch config.TLS.Mode {
+	case TLSModeACME:
+		cfg := autocert.Config{
+			Domains: config.TLS.ACME.Domains,
+		}
+		listener, err = autocert.ListenContext(ctx, network, address, &cfg)
+	case TLSModeStatic:
+		cfg := tls.Config{}
+		listener, err = tls.Listen(network, address, &cfg)
+	default:
+		return nil, fmt.Errorf("unknown TLS mode: %s", config.TLS.Mode)
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	server := Server{
+		logger: logger,
+	}
+	return &server, nil
 }
