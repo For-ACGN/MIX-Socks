18ba0547b560e4982a49f7e96905e4802f54d209
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-07 01:40:57 +0800

    add testRemoveServerLogFile and TestNewServer, finish simple test.

--- server_test.go
+++ server_test.go
@@ -1,1 +1,36 @@
 package msocks
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const testServerLogFile = "testdata/server.log"
+
+func testRemoveServerLogFile(t *testing.T) {
+	err := os.Remove(testServerLogFile)
+	require.NoError(t, err)
+}
+
+func TestNewServer(t *testing.T) {
+	defer testRemoveServerLogFile(t)
+
+	config := ServerConfig{}
+	config.Common.LogPath = testServerLogFile
+	config.Common.PassHash = testPassHash
+	config.HTTP.Network = "tcp"
+	config.HTTP.Address = "127.0.0.1:2019"
+	config.TLS.Mode = TLSModeStatic
+	config.TLS.Static.Cert = "testdata/server_cert.pem"
+	config.TLS.Static.Key = "testdata/server_key.pem"
+
+	server, err := NewServer(context.Background(), &config)
+	require.NoError(t, err)
+	require.NotNil(t, server)
+
+	err = server.Close()
+	require.NoError(t, err)
+}
