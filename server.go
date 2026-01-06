50ad3f94bb7c632aabf4bcb95c1b96bd9e488db8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-07 01:38:54 +0800

    finish NewServer about process TLSModeStatic, add HTTP handler.

--- server.go
+++ server.go
@@ -5,8 +5,12 @@ import (
 	"crypto/tls"
 	"fmt"
 	"net"
+	"net/http"
+	"time"
 
 	"github.com/For-ACGN/autocert"
+	"github.com/pkg/errors"
+	"golang.org/x/net/netutil"
 )
 
 const (
@@ -14,24 +18,25 @@ const (
 	TLSModeStatic = "static"
 )
 
+const (
+	defaultMaxConns = 1000
+	defaultTimeout  = time.Minute
+)
+
 // Server is a SOCKS-over-HTTPS server.
 type Server struct {
-	passHash string
-	logger   *logger
+	logger *logger
+
+	listener net.Listener
+	server   *http.Server
 }
 
 // NewServer is used to create a SoH server.
 func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
 	logger, err := newLogger(config.Common.LogPath)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to open log file")
 	}
-
-	maxConns := config.HTTP.MaxConns
-	if maxConns < 1 {
-		maxConns = 1000
-	}
-
 	network := config.HTTP.Network
 	address := config.HTTP.Address
 	var listener net.Listener
@@ -42,7 +47,14 @@ func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
 		}
 		listener, err = autocert.ListenContext(ctx, network, address, &cfg)
 	case TLSModeStatic:
-		cfg := tls.Config{}
+		kp := config.TLS.Static
+		cert, err := tls.LoadX509KeyPair(kp.Cert, kp.Key)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to load TLS certificate and key")
+		}
+		cfg := tls.Config{
+			Certificates: []tls.Certificate{cert},
+		}
 		listener, err = tls.Listen(network, address, &cfg)
 	default:
 		return nil, fmt.Errorf("unknown TLS mode: %s", config.TLS.Mode)
@@ -50,9 +62,66 @@ func NewServer(ctx context.Context, config *ServerConfig) (*Server, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	server := Server{
-		logger: logger,
+	// apply maximum connections
+	maxConns := config.HTTP.MaxConns
+	if maxConns < 1 {
+		maxConns = defaultMaxConns
 	}
+	listener = netutil.LimitListener(listener, maxConns)
+	// create http server
+	mux := http.NewServeMux()
+	timeout := time.Duration(config.HTTP.Timeout)
+	if timeout < time.Second {
+		timeout = defaultTimeout
+	}
+	srv := http.Server{
+		ReadHeaderTimeout: timeout,
+		ReadTimeout:       timeout,
+		WriteTimeout:      timeout,
+		IdleTimeout:       timeout,
+		Handler:           mux,
+	}
+	server := Server{
+		logger:   logger,
+		listener: listener,
+		server:   &srv,
+	}
+	mux.HandleFunc("/", server.handleIndex)
+	mux.HandleFunc("/"+config.Common.PassHash, server.handleConn)
 	return &server, nil
+}
+
+func (s *Server) handleIndex(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("hello: "))
+	_, _ = w.Write([]byte(r.RemoteAddr))
+}
+
+func (s *Server) handleConn(w http.ResponseWriter, r *http.Request) {
+	hijacker, ok := w.(http.Hijacker)
+	if !ok {
+		s.logger.Errorf("connection from %s can not be hijacked", r.RemoteAddr)
+		return
+	}
+	conn, _, err := hijacker.Hijack()
+	if err != nil {
+		s.logger.Error("failed to hijack connection from", r.RemoteAddr)
+		return
+	}
+	defer func() { _ = conn.Close() }()
+}
+
+// Serve is used to start http server.
+func (s *Server) Serve() error {
+	return s.server.Serve(s.listener)
+}
+
+// Close is used to close http server.
+func (s *Server) Close() error {
+	err := s.logger.Close()
+	e := s.server.Close()
+	if e != nil && err == nil {
+		err = e
+	}
+	return err
 }
