74c1efdc71f869b2daebbbe18548b4ee0ccffd9b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-14 22:17:16 +0800

    update serveHTTPProxy about process request loop, append default port.

--- http_proxy.go
+++ http_proxy.go
@@ -2,10 +2,8 @@ package msocks
 
 import (
 	"bufio"
-	"io"
 	"net"
 	"net/http"
-	"time"
 )
 
 func (c *Client) serveHTTPRequest(conn net.Conn, reader *bufio.Reader) (net.Conn, error) {
@@ -16,11 +14,11 @@ func (c *Client) serveHTTPRequest(conn net.Conn, reader *bufio.Reader) (net.Conn
 	if req.Method == http.MethodConnect {
 		return c.serveHTTPConnect(conn, req)
 	}
-	return c.serveHTTPProxy(conn, req)
+	return c.serveHTTPProxy(conn, reader, req)
 }
 
-func (c *Client) serveHTTPConnect(conn net.Conn, r *http.Request) (net.Conn, error) {
-	tun, err := c.connect("tcp", r.URL.Host)
+func (c *Client) serveHTTPConnect(conn net.Conn, req *http.Request) (net.Conn, error) {
+	tun, err := c.connect("tcp", req.URL.Host)
 	if err != nil {
 		resp := http.Response{}
 		resp.StatusCode = http.StatusBadGateway
@@ -44,33 +42,46 @@ func (c *Client) serveHTTPConnect(conn net.Conn, r *http.Request) (net.Conn, err
 	return tun, nil
 }
 
-func (c *Client) serveHTTPProxy(conn net.Conn, r *http.Request) (net.Conn, error) {
-	resp := http.Response{}
-	resp.StatusCode = http.StatusBadGateway
-	resp.Proto = "HTTP/1.1"
-	resp.ProtoMajor = 1
-	resp.ProtoMinor = 1
+func (c *Client) serveHTTPProxy(conn net.Conn, rd *bufio.Reader, req *http.Request) (net.Conn, error) {
+	badResp := http.Response{}
+	badResp.StatusCode = http.StatusBadGateway
+	badResp.Proto = "HTTP/1.1"
+	badResp.ProtoMajor = 1
+	badResp.ProtoMinor = 1
 
-	port := r.URL.Port()
+	port := req.URL.Port()
 	if port == "" {
 		port = "80"
 	}
-	address := net.JoinHostPort(r.URL.Host, port)
+	address := net.JoinHostPort(req.URL.Host, port)
+
 	tun, err := c.connect("tcp", address)
 	if err != nil {
-		_ = resp.Write(conn)
+		_ = badResp.Write(conn)
 		return nil, err
 	}
 	defer func() { _ = tun.Close() }()
 
-	r.Close = true
-	err = r.Write(tun)
-	if err != nil {
-		_ = resp.Write(conn)
-		return nil, err
+	// process request loop
+	tunBuf := bufio.NewReader(tun)
+	for {
+		err = req.Write(tun)
+		if err != nil {
+			_ = badResp.Write(conn)
+			return nil, err
+		}
+		resp, err := http.ReadResponse(tunBuf, req)
+		if err != nil {
+			_ = badResp.Write(conn)
+			return nil, err
+		}
+		err = resp.Write(conn)
+		if err != nil {
+			return nil, nil
+		}
+		req, err = http.ReadRequest(rd)
+		if err != nil {
+			return nil, nil
+		}
 	}
-
-	_ = tun.SetDeadline(time.Time{})
-	_, _ = io.Copy(conn, tun)
-	return nil, nil
 }
