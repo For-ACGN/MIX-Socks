b9c98b1e79be0666368a459acff348b21294485c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-15 14:32:39 +0800

    add sub test about failed to connect to TestHTTPProxy_ServeHTTPConnect.

--- http_proxy_test.go
+++ http_proxy_test.go
@@ -7,102 +7,202 @@ import (
 	"net/http"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestHTTPProxy_serveHTTPConnect(t *testing.T) {
-	defer func() {
-		testRemoveClientLogFile(t)
-		testRemoveServerLogFile(t)
-	}()
+func TestHTTPProxy_ServeHTTPConnect(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
 
-	serverCfg := testBuildServerConfig()
-	server, err := NewServer(context.Background(), serverCfg)
-	require.NoError(t, err)
-	require.NotNil(t, server)
-	go func() {
-		err := server.Serve()
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
 		require.NoError(t, err)
-	}()
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
 
-	clientCfg := testBuildClientConfig()
-	client, err := NewClient(clientCfg)
-	require.NoError(t, err)
-	require.NotNil(t, client)
-
-	go func() {
-		err := client.Serve()
+		clientCfg := testBuildClientConfig()
+		client, err := NewClient(clientCfg)
 		require.NoError(t, err)
-	}()
+		require.NotNil(t, client)
 
-	transport := http.Transport{
-		Proxy: func(_ *http.Request) (*url.URL, error) {
-			return url.Parse("http://127.0.0.1:2020/")
-		},
-	}
-	httpClient := http.Client{
-		Transport: &transport,
-	}
-	resp, err := httpClient.Get("https://github.com/")
-	require.NoError(t, err)
-	data, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-	fmt.Println(len(data))
-	fmt.Println(string(data))
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
 
-	err = client.Close()
-	require.NoError(t, err)
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("http://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("https://github.com/")
+		require.NoError(t, err)
+		data, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		fmt.Println(len(data))
+		fmt.Println(string(data))
 
-	err = server.Close()
-	require.NoError(t, err)
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to connect", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
+
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
+		require.NoError(t, err)
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
+
+		clientCfg := testBuildClientConfig()
+		clientCfg.Client.Timeout = duration(3 * time.Second)
+
+		client, err := NewClient(clientCfg)
+		require.NoError(t, err)
+		require.NotNil(t, client)
+
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
+
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("http://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("https://invalid-fff17531.com/")
+		require.Error(t, err)
+		require.Nil(t, resp)
+
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
 }
 
-func TestHTTPProxy_serveHTTPProxy(t *testing.T) {
-	defer func() {
-		testRemoveClientLogFile(t)
-		testRemoveServerLogFile(t)
-	}()
+func TestHTTPProxy_ServeHTTPProxy(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
 
-	serverCfg := testBuildServerConfig()
-	server, err := NewServer(context.Background(), serverCfg)
-	require.NoError(t, err)
-	require.NotNil(t, server)
-	go func() {
-		err := server.Serve()
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
 		require.NoError(t, err)
-	}()
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
 
-	clientCfg := testBuildClientConfig()
-	client, err := NewClient(clientCfg)
-	require.NoError(t, err)
-	require.NotNil(t, client)
-
-	go func() {
-		err := client.Serve()
+		clientCfg := testBuildClientConfig()
+		client, err := NewClient(clientCfg)
 		require.NoError(t, err)
-	}()
+		require.NotNil(t, client)
 
-	transport := http.Transport{
-		Proxy: func(_ *http.Request) (*url.URL, error) {
-			return url.Parse("http://127.0.0.1:2020/")
-		},
-	}
-	httpClient := http.Client{
-		Transport: &transport,
-	}
-	resp, err := httpClient.Get("http://github.com/")
-	require.NoError(t, err)
-	data, err := io.ReadAll(resp.Body)
-	require.NoError(t, err)
-	fmt.Println(len(data))
-	fmt.Println(string(data))
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
 
-	err = client.Close()
-	require.NoError(t, err)
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("http://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("http://github.com/")
+		require.NoError(t, err)
+		data, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		fmt.Println(len(data))
+		fmt.Println(string(data))
 
-	err = server.Close()
-	require.NoError(t, err)
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to connect", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
+
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
+		require.NoError(t, err)
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
+
+		clientCfg := testBuildClientConfig()
+		clientCfg.Client.Timeout = duration(3 * time.Second)
+
+		client, err := NewClient(clientCfg)
+		require.NoError(t, err)
+		require.NotNil(t, client)
+
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
+
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("http://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("http://invalid-fff17531.com/")
+		if err == nil {
+			require.Equal(t, http.StatusBadGateway, resp.StatusCode)
+			err = resp.Body.Close()
+			require.NoError(t, err)
+		}
+
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
 }
 
 func TestHTTPProxy_Authenticate(t *testing.T) {
