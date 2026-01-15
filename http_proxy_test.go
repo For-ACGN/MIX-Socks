a4a38b56722163c3f8e7c577e34c6622257ffa86
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-15 14:11:34 +0800

    add TestHTTPProxy_serveHTTPConnect and TestHTTPProxy_serveHTTPProxy.

--- http_proxy_test.go
+++ http_proxy_test.go
@@ -1,1 +1,106 @@
 package msocks
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTTPProxy_serveHTTPConnect(t *testing.T) {
+	defer func() {
+		testRemoveClientLogFile(t)
+		testRemoveServerLogFile(t)
+	}()
+
+	serverCfg := testBuildServerConfig()
+	server, err := NewServer(context.Background(), serverCfg)
+	require.NoError(t, err)
+	require.NotNil(t, server)
+	go func() {
+		err := server.Serve()
+		require.NoError(t, err)
+	}()
+
+	clientCfg := testBuildClientConfig()
+	client, err := NewClient(clientCfg)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	go func() {
+		err := client.Serve()
+		require.NoError(t, err)
+	}()
+
+	transport := http.Transport{
+		Proxy: func(_ *http.Request) (*url.URL, error) {
+			return url.Parse("http://127.0.0.1:2020/")
+		},
+	}
+	httpClient := http.Client{
+		Transport: &transport,
+	}
+	resp, err := httpClient.Get("https://github.com/")
+	require.NoError(t, err)
+	data, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	fmt.Println(len(data))
+	fmt.Println(string(data))
+
+	err = client.Close()
+	require.NoError(t, err)
+
+	err = server.Close()
+	require.NoError(t, err)
+}
+
+func TestHTTPProxy_serveHTTPProxy(t *testing.T) {
+	defer func() {
+		testRemoveClientLogFile(t)
+		testRemoveServerLogFile(t)
+	}()
+
+	serverCfg := testBuildServerConfig()
+	server, err := NewServer(context.Background(), serverCfg)
+	require.NoError(t, err)
+	require.NotNil(t, server)
+	go func() {
+		err := server.Serve()
+		require.NoError(t, err)
+	}()
+
+	clientCfg := testBuildClientConfig()
+	client, err := NewClient(clientCfg)
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	go func() {
+		err := client.Serve()
+		require.NoError(t, err)
+	}()
+
+	transport := http.Transport{
+		Proxy: func(_ *http.Request) (*url.URL, error) {
+			return url.Parse("http://127.0.0.1:2020/")
+		},
+	}
+	httpClient := http.Client{
+		Transport: &transport,
+	}
+	resp, err := httpClient.Get("http://github.com/")
+	require.NoError(t, err)
+	data, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	fmt.Println(len(data))
+	fmt.Println(string(data))
+
+	err = client.Close()
+	require.NoError(t, err)
+
+	err = server.Close()
+	require.NoError(t, err)
+}
