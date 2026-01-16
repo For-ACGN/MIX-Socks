86f2db8d1d02fd88af7c2a50f257b8708450ab51
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-16 14:31:05 +0800

    add TestSOCKS5_ServeSOCKS5, finish all sub tests in it.

--- socks5_test.go
+++ socks5_test.go
@@ -1,1 +1,112 @@
 package msocks
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSOCKS5_ServeSOCKS5(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
+
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
+		require.NoError(t, err)
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
+
+		clientCfg := testBuildClientConfig()
+		client, err := NewClient(clientCfg)
+		require.NoError(t, err)
+		require.NotNil(t, client)
+
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
+
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("socks5://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("https://github.com/")
+		require.NoError(t, err)
+		data, err := io.ReadAll(resp.Body)
+		require.NoError(t, err)
+		fmt.Println(len(data))
+		fmt.Println(string(data))
+
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("failed to connect", func(t *testing.T) {
+		defer func() {
+			testRemoveClientLogFile(t)
+			testRemoveServerLogFile(t)
+		}()
+
+		serverCfg := testBuildServerConfig()
+		server, err := NewServer(context.Background(), serverCfg)
+		require.NoError(t, err)
+		require.NotNil(t, server)
+		go func() {
+			err := server.Serve()
+			require.NoError(t, err)
+		}()
+
+		clientCfg := testBuildClientConfig()
+		clientCfg.Client.Timeout = duration(3 * time.Second)
+
+		client, err := NewClient(clientCfg)
+		require.NoError(t, err)
+		require.NotNil(t, client)
+
+		go func() {
+			err := client.Serve()
+			require.NoError(t, err)
+		}()
+
+		transport := http.Transport{
+			Proxy: func(_ *http.Request) (*url.URL, error) {
+				return url.Parse("socks5://127.0.0.1:2020/")
+			},
+		}
+		httpClient := http.Client{
+			Transport: &transport,
+		}
+		resp, err := httpClient.Get("https://invalid-fff17531.com/")
+		require.Error(t, err)
+		require.Nil(t, resp)
+
+		err = client.Close()
+		require.NoError(t, err)
+
+		err = server.Close()
+		require.NoError(t, err)
+	})
+}
+
+func TestSOCKS5_Authenticate(t *testing.T) {
+
+}
