0613b517b55adb8cd2fd83770e4354ed8494833a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-17 14:03:44 +0800

    implement serveSOCKS4 about SOCKS4 and SOCKS4a, fix socks4CheckUserID.

--- socks4.go
+++ socks4.go
@@ -2,9 +2,12 @@ package msocks
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/subtle"
+	"encoding/binary"
 	"io"
 	"net"
+	"strconv"
 
 	"github.com/pkg/errors"
 )
@@ -18,8 +21,13 @@ const (
 	v4Connect   = 0x01
 	v4Succeeded = 0x5A
 	v4Refused   = 0x5B
-	v4Ident     = 0x5C
 	v4InvalidID = 0x5D
+)
+
+var (
+	v4ReplySucceeded = []byte{0x00, v4Succeeded, 0, 0, 0, 0, 0, 0}
+	v4ReplyRefused   = []byte{0x00, v4Refused, 0, 0, 0, 0, 0, 0}
+	v4ReplyInvalidID = []byte{0x00, v4InvalidID, 0, 0, 0, 0, 0, 0}
 )
 
 func (c *Client) serveSOCKS4(conn net.Conn, reader *bufio.Reader) (net.Conn, error) {
@@ -35,17 +43,66 @@ func (c *Client) serveSOCKS4(conn net.Conn, reader *bufio.Reader) (net.Conn, err
 		c.logger.Error("")
 		return nil, errors.New("unexpected socks4 version")
 	}
-	// command
+	// check command
 	if buf[1] != v4Connect {
 		return nil, errors.Errorf("unknown command: %d", buf[1])
 	}
-	if !c.checkUserID(reader) {
+	if !c.socks4CheckUserID(reader) {
+		_, _ = conn.Write(v4ReplyInvalidID)
 		return nil, errors.New("failed to check user id")
 	}
-	return nil, nil
+	// process connect target
+	port := binary.BigEndian.Uint16(buf[2:4])
+	var (
+		domain bool
+		ip     bool
+		host   string
+		proto  string
+	)
+	// check is domain, 0.0.0.x is domain mode
+	if bytes.Equal(buf[4:7], []byte{0x00, 0x00, 0x00}) && buf[7] != 0x00 {
+		domain = true
+		proto = "SOCKS4a"
+	} else {
+		ip = true
+		proto = "SOCKS4"
+	}
+	if ip {
+		host = net.IPv4(buf[4], buf[5], buf[6], buf[7]).String()
+	}
+	// read domain
+	if domain {
+		var domainName []byte
+		for {
+			b, err := reader.ReadByte()
+			if err != nil {
+				return nil, errors.Wrap(err, "failed to read domain name")
+			}
+			// find 0x00(end)
+			if b == 0x00 {
+				break
+			}
+			domainName = append(domainName, b)
+		}
+		host = string(domainName)
+	}
+	target := net.JoinHostPort(host, strconv.Itoa(int(port)))
+	// connect target
+	tun, err := c.connect(proto, "tcp", target)
+	if err != nil {
+		_, _ = conn.Write(v4ReplyRefused)
+		return nil, errors.Wrap(err, "failed to connect target")
+	}
+	// write reply
+	_, err = conn.Write(v4ReplySucceeded)
+	if err != nil {
+		_ = tun.Close()
+		return nil, errors.Wrap(err, "failed to write reply")
+	}
+	return tun, nil
 }
 
-func (c *Client) checkUserID(reader *bufio.Reader) bool {
+func (c *Client) socks4CheckUserID(reader *bufio.Reader) bool {
 	var userID []byte
 	for {
 		b, err := reader.ReadByte()
@@ -65,7 +122,7 @@ func (c *Client) checkUserID(reader *bufio.Reader) bool {
 	}
 	uid := []byte(c.frontUsername)
 	if subtle.ConstantTimeCompare(uid, userID) != 1 {
-		c.logger.Error("invalid user id: %s", userID)
+		c.logger.Errorf("invalid user id: %s", userID)
 		return false
 	}
 	return true
