4ebc6a13333a647023cb198314caa6c192539e17
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-04-26 13:02:25 +0800

    improve newMathRand about fallback when failed to read random bytes.

--- rand.go
+++ rand.go
@@ -2,8 +2,11 @@ package msocks
 
 import (
 	crand "crypto/rand"
+	"crypto/sha256"
 	"encoding/binary"
 	"math/rand"
+	"os"
+	"runtime"
 	"time"
 )
 
@@ -13,8 +16,20 @@ func newMathRand() *mathRand {
 	buf := make([]byte, 8)
 	_, err := crand.Read(buf)
 	if err != nil {
-		now := time.Now().UnixNano()
-		binary.BigEndian.PutUint64(buf, uint64(now)) // #nosec G115
+		hash := sha256.New()
+		binary.BigEndian.PutUint64(buf, uint64(time.Now().UnixNano()))
+		hash.Write(buf)
+		binary.BigEndian.PutUint64(buf, uint64(os.Getpid()))
+		hash.Write(buf)
+		var m runtime.MemStats
+		runtime.ReadMemStats(&m)
+		binary.BigEndian.PutUint64(buf, m.HeapAlloc)
+		hash.Write(buf)
+		binary.BigEndian.PutUint64(buf, m.NextGC)
+		hash.Write(buf)
+		binary.BigEndian.PutUint64(buf, uint64(m.NumGC))
+		hash.Write(buf)
+		buf = hash.Sum(nil)[:8]
 	}
 	seed := binary.BigEndian.Uint64(buf)
 	return rand.New(rand.NewSource(int64(seed))) // #nosec
